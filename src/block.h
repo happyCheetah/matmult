3d256b7ed7cf159c994121e4bf37301bd806a6ac
CatherineMeng <31551894+CatherineMeng@users.noreply.github.com>
2024-03-01 12:46:18 -0800

    Update block.h

--- src/block.h
+++ src/block.h
@@ -6,15 +6,14 @@
 using namespace std;
 
 const int SIZE = 32;
-const int BLOCK_SIZE = 32;
 
 typedef struct {
-	int a[BLOCK_SIZE]; 
+	int a[SIZE]; 
 } blockvec;
 
-typedef struct {
-	int out[BLOCK_SIZE][BLOCK_SIZE]; //one output block of matrix C
-} blockmat;
+// typedef struct {
+// 	int out[SIZE][SIZE]; //one output block of matrix C
+// } blockmat;
 
 void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, int it);
 void blockmatmul(hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols,
