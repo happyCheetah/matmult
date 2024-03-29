bbaa48b7db509e26872f3fcaed00cdfda2a81f4c
happyCheetah <mguo2003@gmail.com>
2024-03-29 13:00:14 -0700

    Update block.h

--- src/block.h
+++ src/block.h
@@ -15,6 +15,7 @@ typedef struct {
 // 	int out[SIZE][SIZE]; //one output block of matrix C
 // } blockmat;
 
-void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, int it);
+//void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, int it);
+void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, blockvec Bcols[], int it)
 //void blockmatmul(hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, blockvec C[], int it);
 void blockmatmul(hls::stream<blockvec> &Arows, blockvec Bcols[], blockvec C[], int it);
