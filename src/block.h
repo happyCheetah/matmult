2e25214a62e478abfb2977e366ca048018a33f73
happyCheetah <mguo2003@gmail.com>
2024-07-17 22:15:13 -0700

    Update block.h

--- src/block.h
+++ src/block.h
@@ -5,17 +5,17 @@
 #include <vector>
 using namespace std;
 
-const int SIZE = 32;
+const int SIZE = 1024;
+const int BLOCK_SIZE = 16;
 
 typedef struct {
-	int a[SIZE]; 
+	int a[BLOCK_SIZE]; 
 } blockvec;
 
-// typedef struct {
-// 	int out[SIZE][SIZE]; //one output block of matrix C
-// } blockmat;
+typedef struct {
+	int out[BLOCK_SIZE][BLOCK_SIZE]; //one output block of matrix C
+} blockmat;
 
-//void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, int it);
-void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, blockvec Bcols[], int it);
-//void blockmatmul(hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, blockvec C[], int it);
-void blockmatmul(hls::stream<blockvec> &Arows, blockvec Bcols[], blockvec C[], int it);
+void loadDDR(blockvec A[], blockvec B[], hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols, int it);
+void blockmatmul(hls::stream<blockvec> &Arows, hls::stream<blockvec> &Bcols,
+blockmat & ABpartial, int it);
